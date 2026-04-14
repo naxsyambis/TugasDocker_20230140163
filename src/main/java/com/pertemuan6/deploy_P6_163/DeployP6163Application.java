059dbb7ce97245c75ca823582ab858aeaca7f426
naxsyambis <naxsyambis@gmail.com>
2026-04-14 13:47:29 +0700

    Initial commit

---
+++ src/main/java/com/pertemuan6/deploy_P6_163/DeployP6163Application.java
@@ -0,0 +1,13 @@
+package com.pertemuan6.deploy_P6_163;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployP6163Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployP6163Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pertemuan6/deploy_P6_163/DeployP6163ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pertemuan6.deploy_P6_163;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployP6163ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
